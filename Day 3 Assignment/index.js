41132450cb3840c225e806dbcb6056b66b2b65fc
Mariya <88597972+mariya9612@users.noreply.github.com>
2021-09-09 08:52:01 +0530

    Add files via upload

---
+++ Day 3 Assignment/index.js
@@ -0,0 +1,15 @@
+function addTask(){
+    let data=document.getElementById(`inp`).value;
+     document.getElementById("two").innerHTML+=`<h1 class='todo'>${data}</h1>`;
+     document.getElementById(`inp`).value="";
+   
+    
+    
+}
+
+
+function changeColor(){
+    document.getElementById('two').style.backgroundColor="rgb(149, 168, 192)";
+}
+
+
